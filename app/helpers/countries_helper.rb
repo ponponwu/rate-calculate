88120f106f5636eabfe27eb61c7ee66928df9e5a
ponpon <sheep0406@hotmail.com>
2016-04-26 17:05:54 +0800

    fix wrong typing TWD-->NTD

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -5,8 +5,8 @@ module CountriesHelper
   end
 
   def my_country_select(title, target_id , objs)
-    html = '<div class="clearfix"><label for="normalSelect">'+title+'</label><div class="input">'
-    html += '<select name="normalSelect" id="'+target_id+'">'
+    html = '<div class="clearfix"><label for='+target_id+'">'+title+'</label><div class="input">'
+    html += '<select id="'+target_id+'">'
     objs.each do |obj|
       html += '<option value="'+obj[1]+'">'+obj[0]+'</option>'
     end
