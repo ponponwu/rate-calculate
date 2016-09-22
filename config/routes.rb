9aec0edf827221eb8a5363275ea1e8ab4ede9477
ponpon <sheep0406@hotmail.com>
2016-09-22 12:56:02 +0800

    Fix modal login issue

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       get :select
     end
   end
-  devise_for :users
+  devise_for :users, :controllers => { sessions: "sessions" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
