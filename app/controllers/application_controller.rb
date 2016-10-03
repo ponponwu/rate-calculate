c657d904f499bed2ba4bac457492d99a34bb61a8
ponpon <sheep0406@hotmail.com>
2016-10-04 01:35:01 +0800

    Add i18n, nav a color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    # 可以將 ["en", "zh-TW"] 設定為 VALID_LANG 放到 config/environment.rb 中
+    if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
   def getCompare(compare, *arr)
     bank = ''
     max = arr[0][0][:buy]
     min = arr[0][0][:sell]
-
-    if compare==0
+    if compare == 0
       bank = arr[0][0][:bank]
       arr[0].each_with_index do |r, idx|
         val = r[:buy]
@@ -16,17 +26,17 @@ class ApplicationController < ActionController::Base
           bank = r[:bank]
         end
       end
-      [bank,max]
+      [bank, max]
     else
       bank = arr[0][0][:bank]
       arr[0].each_with_index do |r, idx|
         val = r[:sell]
-        if val<min
+        if val < min
           min = val
           bank = r[:bank]
         end
       end
-      [bank,min]
+      [bank, min]
     end
   end
   def set_newest_session_rate
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,8 @@
+# reference to country_data
 class Country < ActiveRecord::Base
   has_many :attention
   def self.categories
-    CountryData.country_categories.map{ |k,v| [I18n.t("country_categories.#{k}"),v] }
+    CountryData.country_categories.map{ |k, v| [I18n.t("country_categories.#{k}"), k]}
   end
 
 end
